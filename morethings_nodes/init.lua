0aebc5bfa28b59de2450fa2656fbc62c0e86bb25
fridolin /fridolin2 <lars-ole99@gmx.de>
2017-10-25 19:09:39 +0200

    Add files via upload

--- morethings_nodes/init.lua
+++ morethings_nodes/init.lua
@@ -297,7 +297,30 @@ minetest.register_node("morethings_nodes:carpet_orange", {
 	groups = {cracky = 3, },
 	sounds = default.node_sound_stone_defaults()
 })
+
 -------Define Mossy Blocks
+
+minetest.register_node("morethings_nodes:moss", {
+	description = "Moss",
+	drawtype = "signlike",
+	tiles = {"morethings_moss.png"},
+	inventory_image = "morethings_moss.png",
+	wield_image = "morethings_moss.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {cracky = 2},
+	sounds = default.node_sound_leaves_defaults(),
+})
 
 minetest.register_node("morethings_nodes:mossy_stonebrick", {
 	description = "Mossy Stonebrick",
@@ -539,6 +562,145 @@ minetest.register_node("morethings_nodes:building_lightblock", {
 })
 
 --Define Block Crafting recipes
+
+
+---Moss Recipes
+
+minetest.register_craft({
+	output = "morethings_nodes:moss",
+	recipe = {
+		{"", "group:leaves", ""},
+		{"group:leaves", "", "group:leaves"},
+		{"", "group:leaves", ""}
+	}
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_stone_tile",
+	recipe = {"morethings_nodes:moss", "moreblocks:stone_tile"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_circle_stone",
+	recipe = {"morethings_nodes:moss", "moreblocks:circle_stone_bricks"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_coal_stone",
+	recipe = {"morethings_nodes:moss", "moreblocks:coal_stone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_coal_stone_bricks",
+	recipe = {"morethings_nodes:moss", "moreblocks:coal_stone_bricks"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_desert_sandstone",
+	recipe = {"morethings_nodes:moss", "default:desert_sandstone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_desert_sandstone_block",
+	recipe = {"morethings_nodes:moss", "default:desert_sandstone_block"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_desert_sandstone_brick",
+	recipe = {"morethings_nodes:moss", "default:desert_sandstone_brick"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_desert_stone",
+	recipe = {"morethings_nodes:moss", "default:desert_stone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_desert_stone_block",
+	recipe = {"morethings_nodes:moss", "default:desert_stone_block"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_desert_stonebrick",
+	recipe = {"morethings_nodes:moss", "default:desert_stonebrick"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_iron_stone",
+	recipe = {"morethings_nodes:moss", "moreblocks:iron_stone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_iron_stone_bricks",
+	recipe = {"morethings_nodes:moss", "moreblocks:iron_stone_bricks"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_sandstone",
+	recipe = {"morethings_nodes:moss", "default:sandstone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_sandstone_block",
+	recipe = {"morethings_nodes:moss", "default:sandstone_block"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_sandstone_brick",
+	recipe = {"morethings_nodes:moss", "default:sandstonebrick"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_silver_sandstone",
+	recipe = {"morethings_nodes:moss", "default:silver_sandstone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_silver_sandstone_block",
+	recipe = {"morethings_nodes:moss", "default:silver_sandstone_block"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_silver_sandstone_brick",
+	recipe = {"morethings_nodes:moss", "default:silver_sandstone_brick"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_stone",
+	recipe = {"morethings_nodes:moss", "default:stone"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_stone_block",
+	recipe = {"morethings_nodes:moss", "default:stone_block"},
+})
+
+minetest.register_craft( {
+	type = "shapeless",
+	output = "morethings_nodes:mossy_stonebrick",
+	recipe = {"morethings_nodes:moss", "default:stonebrick"},
+})
+
 
 minetest.register_craft({
 	output = "morethings_nodes:resource_block_red",
